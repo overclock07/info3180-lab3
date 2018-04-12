d585a83fad49d9db555218191e0940478c38eb22
overclock07 <sunjen08@gmail.com>
2018-04-12 12:20:35 +0000

    your commit message

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+from app import views
+app.config['SECRET_KEY'] = '6ab1f1d9bf1b73113fbecda5faf88979'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '465'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+mail = Mail(app)
 from app import views
--- app/views.py
+++ app/views.py
@@ -4,8 +4,10 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import smtplib
+from flask_mail import Message
 from app import app
+from forms import MessageForm
 from flask import render_template, request, redirect, url_for, flash
 
 
@@ -24,6 +26,26 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    """Render the website's contact page."""
+   
+    contact_form = MessageForm()
+    
+    if request.method == 'POST':
+        if contact_form.validate_on_submit():
+            # Get validated data from form
+            name = contact_form["name"]
+            email = contact_form["email"]
+            subject = contact_form["subject"]
+            message = contact_form["message"]
+            
+            send_email(name, email, subject, message)
+            
+            flash('Message Sent')
+            return redirect(url_for('home'))
+            
+    return render_template('contact.html', form=contact_form)
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -52,6 +74,25 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
+    
+def send_email(from_name, from_email, subject, msg):
+    
+    to_name = ""
+    to_addr = ''
+    message = """From: {} <{}> To: {} <{}>Subject: {} {}"""
+
+    message_to_send = message.format(from_name, from_email, to_name,to_addr, subject, msg)
+
+    username = ''
+    password = ''
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email, to_addr, message_to_send)
+    server.quit()
+
 
 
 if __name__ == '__main__':
